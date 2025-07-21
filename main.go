cdda11aba1c613fd2d554b25a99638d400adaa8f
Roddyck <rodion.ivashchenko2003@gmail.com>
2025-07-21 16:35:52 +0300

    update: admin panel, metrics, and validate chirp endpoint

--- main.go
+++ main.go
@@ -3,21 +3,45 @@ package main
 import (
 	"log"
 	"net/http"
+	"sync/atomic"
 )
+
+type apiConfig struct {
+	fileserverHits atomic.Int32
+}
+
+func (cfg *apiConfig) middlewareMetricsInc(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		cfg.fileserverHits.Add(1)
+		next.ServeHTTP(w, r)
+	})
+}
+
 
 func main() {
 	mux := http.NewServeMux()
+	cfg := apiConfig{
+		fileserverHits: atomic.Int32{},
+	}
+
 	httpServer := http.Server{
 		Handler: mux,
 		Addr:    ":8080",
 	}
 
-	mux.Handle("GET /app/", http.StripPrefix("/app", http.FileServer(http.Dir("."))))
-	mux.HandleFunc("/healthz", func(w http.ResponseWriter, r *http.Request) {
+	fileserverHandler := http.StripPrefix("/app", http.FileServer(http.Dir(".")))
+	mux.Handle("/app/", cfg.middlewareMetricsInc(fileserverHandler))
+
+	mux.HandleFunc("GET /api/healthz", func(w http.ResponseWriter, r *http.Request) {
 		w.Header().Add("Content-Type", "text/plain; charset=utf-8")
 		w.WriteHeader(200)
 		w.Write([]byte("OK"))
 	})
+	mux.HandleFunc("POST /api/validate_chirp", handleValidateChirp)
+
+	mux.HandleFunc("GET /admin/metrics", cfg.getMetrics)
+	mux.HandleFunc("POST /admin/reset", cfg.reset)
 
 	log.Fatal(httpServer.ListenAndServe())
 }
+
